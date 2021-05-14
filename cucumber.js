bd9f33dbda83a347238ce8f5989d4b1fc577d479
Michael Naguit <jonan.naguit@gmail.com>
2021-05-15 09:57:59 +1200

    update

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,7 @@ let common = [
     'features/**/*.feature',                // Specify our feature files
     '--require-module ts-node/register',    // Load TypeScript module
     '--require step-definitions/**/*.ts',   // Load step definitions
+    '--format progress-bar',                // Load custom formatter
     '--publish-quiet'                       // Cucumber Publish Message disabled
   ].join(' ');
   
